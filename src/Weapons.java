cd68c81565634bdb436042c71323d7024335bc9a
Michael Lopez <hlopez1@ggc.edu>
2017-10-26 19:21:45 -0400

    Updated Methods

--- src/Weapons.java
+++ src/Weapons.java
@@ -1,4 +1,15 @@
 
 public class Weapons extends Items{
 
+	private int attackPoints;
+
+	public int getAttackPoints() {
+		return attackPoints;
+	}
+
+	public void setAttackPoints(int attackPoints) {
+		this.attackPoints = attackPoints;
+	}
+	
+	
 }
