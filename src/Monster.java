3692b2533f4483c3530065e5e816b427c2a6e0cf
= <=>
2017-11-05 22:37:49 -0500

    added puzzle and monster

--- src/Monster.java
+++ src/Monster.java
@@ -3,60 +3,144 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * 
- */
-
 public class Monster {
-	private String name;
-	private String description;
-	private String health;
+	private String monsterID;
+	private String monsterName;
+	private String monsterDescription;
+	private String EXP;
+	private int damageGiven;
+	private int healthPoints;
+	private int attackPercentage;
+	private String artifactsDropped;
 	private static ArrayList<Monster> monstersArray = new ArrayList<>();
+
+
+	public Monster(String monsterID, String monsterName, String monsterDescription,
+			String EXP, int damageGiven, int healthPoints, int attackPercentage, 
+			String artifactsDropped)
+	{
+		this.monsterID = monsterID;
+		this.monsterName = monsterName;
+		this.monsterDescription = monsterDescription;
+		this.EXP = EXP;
+		this.damageGiven = damageGiven;
+		this.healthPoints = healthPoints;
+		this.attackPercentage = attackPercentage;
+		this.artifactsDropped = artifactsDropped;
+	}
+
+	public String getMonsterID() {
+		return monsterID;
+	}
+
+	public void setMonsterID(String monsterID) {
+		this.monsterID = monsterID;
+	}
+
+	public String getMonsterName() {
+		return monsterName;
+	}
+
+	public void setMonsterName(String monsterName) {
+		this.monsterName = monsterName;
+	}
+
+	public String getMonsterDescription() {
+		return monsterDescription;
+	}
+
+	public void setMonsterDescription(String monsterDescription) {
+		this.monsterDescription = monsterDescription;
+	}
+
+	public String getEXP() {
+		return EXP;
+	}
+
+	public void setEXP(String eXP) {
+		EXP = eXP;
+	}
+
+	public int getDamageGiven() {
+		return damageGiven;
+	}
+
+	public void setDamageGiven(int damageGiven) {
+		this.damageGiven = damageGiven;
+	}
+
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+
+	public int getAttackPercentage() {
+		return attackPercentage;
+	}
+
+	public void setAttackPercentage(int attackPercentage) {
+		this.attackPercentage = attackPercentage;
+	}
+
+	public String getArtifactsDropped() {
+		return artifactsDropped;
+	}
+
+	public void setArtifactsDropped(String artifactsDropped) {
+		this.artifactsDropped = artifactsDropped;
+	}
 
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("monsters");
 		Monster m = new Monster();
-		m.monsterReader();
+		//m.monsterReader();
 	}
 
-	@Override
-	public String toString() {
+
+	/*public String toString() {
 		return name + " | " + description + " | " + health + " | ";
 	}
 
 	public Monster() {
 
 	}
+	 */
 
-	public Monster(String name, String description, String health) {
-		this.name = name;
-		this.description = description;
-		this.health = health;
+	public Monster() {
+
 	}
 
 	public void printMonster() {
 		System.out.println("monsters description");
 	}
-	
-//this method reads the text file and make monster object and 
-//assigns the name,description and health to the object
-	public void monsterReader() throws FileNotFoundException {
+
+	//this method reads the text file and make monster object and 
+	//assigns the name,description and health to the object
+
+	/*public void monsterReader() throws FileNotFoundException {
 		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(new File("monster.txt"));
 		;
 
 		while (reader.hasNext()) {
-			String name = reader.nextLine();
-			String description = reader.nextLine();
-			String health = reader.nextLine();
+			String monsterID = reader.nextLine();
+			String monsterName = reader.nextLine();
+			String monsterDescription = reader.nextLine();
+			String EXP = reader.nextLine();
+			String damageGiven= reader.nextLine();
+			
 
-			Monster monster = new Monster(name, description, health);
-			monstersArray.add(monster);
+			//Monster monster = new Monster(name, description, health);
+			//monstersArray.add(monster);
 		}
-		System.out.println("" + monstersArray.get(0).description);
-		System.out.println("" + monstersArray.get(1).description);
+		System.out.println("" + monstersArray.get(0).monsterDescription);
+		System.out.println("" + monstersArray.get(1).monsterDescription);
 		System.out.println(monstersArray.toString());
 
+*/
 	}
 
-}
+
