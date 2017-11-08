a0213fbc3a4e68fdbabd00d7cadd02273ef999a9
Michael Lopez <hlopez1@ggc.edu>
2017-11-08 14:53:34 -0500

    All weapon items created

--- src/Weapons.java
+++ src/Weapons.java
@@ -1,16 +1,54 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Weapons extends Items{
 
 	private int attackPoints;
+	private static ArrayList<Weapons> weaponsArray = new ArrayList<>();
+	
 
 	public int getAttackPoints() {
 		return attackPoints;
-		//sysout
 	}
 
 	public void setAttackPoints(int attackPoints) {
 		this.attackPoints = attackPoints;
 	}
 	
+	public void EquipItem() { //Placeholder
+		
+	}
 	
+	public void UnequipItem() { //Placeholder
+		
+	}
+	
+	public Weapons(String itemID, String itemName, String itemDescription, int attackPoints) {
+		setItemID(itemID);
+		setItemName(itemName);
+		setItemDescription(itemDescription);
+		setAttackPoints(attackPoints);
+	}
+	
+	
+	
+	public static void weaponsReader() throws FileNotFoundException {
+		@SuppressWarnings("resource")
+		Scanner reader = new Scanner(new File("weapons.txt"));
+		;
+
+		while (reader.hasNext()) {
+			String itemID = reader.nextLine();
+			String itemName = reader.nextLine();
+			String itemDescription = reader.nextLine();
+			String attackPoints = reader.nextLine();
+			Integer result = Integer.valueOf(attackPoints);
+			
+			Weapons weapons = new Weapons(itemID, itemName, itemDescription, result);
+			weaponsArray.add(weapons);
+		}
+		
+	}
 }
