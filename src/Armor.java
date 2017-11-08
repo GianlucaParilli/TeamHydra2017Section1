821c18da1ef554bcdc63b2acab934c18815ab027
Michael Lopez <hlopez1@ggc.edu>
2017-11-08 14:54:25 -0500

    Armor items created

--- src/Armor.java
+++ src/Armor.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Armor extends Items{
 
 	private int defensePoints;
+	private static ArrayList<Armor> armorArray = new ArrayList<>();
+	
 
 	public int getDefensePoints() {
 		return defensePoints;
@@ -11,4 +17,39 @@ public class Armor extends Items{
 		this.defensePoints = defensePoints;
 	}
 	
+	public void EquipItem() { //Placeholder
+		
+	}
+	
+	public void UnequipItem() { //Placeholder
+		
+	}
+	
+	public Armor(String itemID, String itemName, String itemDescription, int defensePoints) {
+		setItemID(itemID);
+		setItemName(itemName);
+		setItemDescription(itemDescription);
+		setDefensePoints(defensePoints);
+	}
+	
+	
+	
+	public static void armorReader() throws FileNotFoundException {
+		@SuppressWarnings("resource")
+		Scanner reader = new Scanner(new File("armor.txt"));
+		;
+
+		while (reader.hasNext()) {
+			String itemID = reader.nextLine();
+			String itemName = reader.nextLine();
+			String itemDescription = reader.nextLine();
+			String defensePoints = reader.nextLine();
+			Integer result = Integer.valueOf(defensePoints);
+			
+			Armor armor = new Armor(itemID, itemName, itemDescription, result);
+			armorArray.add(armor);
+		}
+
+	}
+
 }
