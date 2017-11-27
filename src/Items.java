815fefcfb5e9b6d5fa539ce3a09eb627c024ae1e
Luca Parilli <gluka47@hotmail.com>
2017-11-26 19:24:09 -0500

    removed import

--- src/Items.java
+++ src/Items.java
@@ -8,7 +8,6 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
